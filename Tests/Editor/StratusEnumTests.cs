30ae0bb90a72ea59a7864095df72b63e85796fec
Christian Sagel <ckpsm@live.com>
2022-01-16 21:43:05 +0100

    Removed namespaces

--- Tests/Editor/StratusEnumTests.cs
+++ Tests/Editor/StratusEnumTests.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
+﻿using NUnit.Framework;
 
-using NUnit.Framework;
 using System.Linq;
-
-using UnityEngine;
 
 namespace Stratus.Editor.Tests
 {
