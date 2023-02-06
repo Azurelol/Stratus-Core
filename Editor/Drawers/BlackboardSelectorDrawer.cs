ce0b10ca3f606409ec3ebbc4e5a5f99c1b2679c4
Christian Sagel <ckpsm@live.com>
2023-02-07 00:18:53 +0100

    Refactored blackboard from Core to NET

--- Editor/Drawers/BlackboardSelectorDrawer.cs
+++ Editor/Drawers/BlackboardSelectorDrawer.cs
@@ -9,42 +9,42 @@ using UnityEngine;
 
 namespace Stratus
 {
-	[CustomPropertyDrawer(typeof(StratusBlackboard.Selector))]
-  public class BlackboardSelectorDrawer : ObjectPropertySelectorDrawer<StratusBlackboard.Selector>
-  {
-    protected override string objectPropertyName { get; } = nameof(StratusBlackboard.Selector.blackboard);
-    protected override void DrawSelector(Rect position, SerializedProperty property, StratusBlackboard.Selector target)
-    {
-      // Select the scope
-      SerializedProperty scopeProperty = property.FindPropertyRelative(nameof(StratusBlackboard.Selector.scope));
-      position.width /= 2f;
-      EditorGUI.PropertyField(position, scopeProperty, GUIContent.none);
-      position.x += position.width;
+	[CustomPropertyDrawer(typeof(Blackboard.Selector))]
+	public class BlackboardSelectorDrawer : ObjectPropertySelectorDrawer<Blackboard.Selector>
+	{
+		protected override string objectPropertyName { get; } = nameof(Blackboard.Selector.blackboard);
+		protected override void DrawSelector(Rect position, SerializedProperty property, Blackboard.Selector target)
+		{
+			// Select the scope
+			SerializedProperty scopeProperty = property.FindPropertyRelative(nameof(Blackboard.Selector.scope));
+			position.width /= 2f;
+			EditorGUI.PropertyField(position, scopeProperty, GUIContent.none);
+			position.x += position.width;
 
-      //Types.Symbol[] symbols = null;
-      string[] keys = null;
-      switch (target.scope)
-      {
-        case StratusBlackboard.Scope.Local:
-          keys = target.blackboard.locals.keys;
-          break;
-        case StratusBlackboard.Scope.Global:
-          keys = target.blackboard.globals.keys;
-          break;
-      }
+			//Types.Symbol[] symbols = null;
+			string[] keys = null;
+			switch (target.scope)
+			{
+				case Blackboard.Scope.Local:
+					keys = target.blackboard.locals.keys;
+					break;
+				case Blackboard.Scope.Global:
+					keys = target.blackboard.globals.keys;
+					break;
+			}
 
-      // Select the key
-      SerializedProperty keyProperty = property.FindPropertyRelative(nameof(StratusBlackboard.Selector.key));
-      int index = keys.FindIndex(keyProperty.stringValue);
-      EditorGUI.BeginChangeCheck();
-      index = EditorGUI.Popup(position, index, keys);
-      if (EditorGUI.EndChangeCheck())
-      {
-        keyProperty.stringValue = keys[index];
-      }
-      
-    }
+			// Select the key
+			SerializedProperty keyProperty = property.FindPropertyRelative(nameof(Blackboard.Selector.key));
+			int index = keys.FindIndex(keyProperty.stringValue);
+			EditorGUI.BeginChangeCheck();
+			index = EditorGUI.Popup(position, index, keys);
+			if (EditorGUI.EndChangeCheck())
+			{
+				keyProperty.stringValue = keys[index];
+			}
 
-  }
+		}
+
+	}
 
 }
--- Samples/Scripts/BlackboardExample.cs
+++ Samples/Scripts/BlackboardExample.cs
@@ -5,21 +5,21 @@ namespace Stratus.Samples
 	public class BlackboardExample : StratusBehaviour
 	{
 		[Header("Default")]
-		public StratusBlackboard blackboard;
-		public StratusBlackboard.Scope scope;
+		public BlackboardScriptable blackboard;
+		public Blackboard.Scope scope;
 		public string key = "Dogs";
 		public int intValue = 5;
 
 		[Header("Selector")]
-		public StratusBlackboard.Selector selector = new StratusBlackboard.Selector();
+		public Blackboard.Selector selector = new Blackboard.Selector();
 		public StratusRuntimeMethodField runtimeMethod;
 
 
 		private void Awake()
 		{
 			runtimeMethod = new StratusRuntimeMethodField(GetValue, GetValueWithSelector, SetValue);
-			blackboard.onLocalSymbolChanged += OnLocalSymbolChanged;
-			blackboard.onGlobalSymbolChanged += OnGlobalSymbolChanged;
+			blackboard.blackboard.onLocalSymbolChanged += OnLocalSymbolChanged;
+			blackboard.blackboard.onGlobalSymbolChanged += OnGlobalSymbolChanged;
 		}
 
 		// Examples -----------------------------------------------------------
@@ -31,11 +31,11 @@ namespace Stratus.Samples
 			object value = null;
 			switch (scope)
 			{
-				case StratusBlackboard.Scope.Local:
-					value = blackboard.GetLocal(gameObject, key);
+				case Blackboard.Scope.Local:
+					value = blackboard.blackboard.GetLocal(gameObject, key);
 					break;
-				case StratusBlackboard.Scope.Global:
-					value = blackboard.GetGlobal(key);
+				case Blackboard.Scope.Global:
+					value = blackboard.blackboard.GetGlobal(key);
 					break;
 			}
 			StratusDebug.Log($"The value of {key} is {value}", this);
@@ -55,14 +55,14 @@ namespace Stratus.Samples
 			// Example of how such a value would be set...
 			switch (scope)
 			{
-				case StratusBlackboard.Scope.Local:
+				case Blackboard.Scope.Local:
 					// ... to the table for local symbols in the blackboard, instantiated for
 					// each GameObject on access
-					blackboard.SetLocal(gameObject, key, intValue);
+					blackboard.blackboard.SetLocal(gameObject, key, intValue);
 					break;
-				case StratusBlackboard.Scope.Global:
+				case Blackboard.Scope.Global:
 					// ... to the table for global symbols in the blackboard
-					blackboard.SetGlobal(key, intValue);
+					blackboard.blackboard.SetGlobal(key, intValue);
 					break;
 			}
 
@@ -77,7 +77,7 @@ namespace Stratus.Samples
 		//--------------------------------------------------------------------/
 		// Callbacks
 		//--------------------------------------------------------------------/
-		private void OnLocalSymbolChanged(GameObject gameObject, StratusSymbol symbol)
+		private void OnLocalSymbolChanged(object gameObject, StratusSymbol symbol)
 		{
 			StratusDebug.Log($"The value on local symbol {symbol.key} on the GameObject {gameObject} was changed to {symbol.value}", this);
 		}
