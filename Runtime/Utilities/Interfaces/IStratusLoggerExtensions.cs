782aa649c6e06f8c47a40ce544d96ec26f67ebcc
Christian Sagel <ckpsm@live.com>
2023-02-11 23:01:52 +0100

    Moved the logger extensions onto the NET library

--- Runtime/Utilities/Interfaces/IStratusLoggerExtensions.cs
+++ Runtime/Utilities/Interfaces/IStratusLoggerExtensions.cs
@@ -3,61 +3,65 @@ using UnityEngine;
 
 namespace Stratus
 {
-	public static class IStratusLoggerExtensions
-    {
-		/// <summary>
-		/// Prints the given message to the console
-		/// </summary>
-		/// <param name="value"></param>
-		public static void Log(this IStratusLogger logger, object value) => StratusDebug.Log(value, logger, 2);
+	//public static class IStratusLoggerExtensions
+ //   {
+	//	/// <summary>
+	//	/// Prints the given message to the console
+	//	/// </summary>
+	//	/// <param name="value"></param>
+	//	public static void Log(this IStratusLogger logger, object value) => StratusDebug.Log(value, logger, 2);
 
-		/// <summary>
-		/// Prints the given message to the console
-		/// </summary>
-		/// <param name="value"></param>
-		public static void LogIf(this IStratusLogger logger, bool condition, object value) => StratusDebug.LogIf(condition, value, logger, 2);
+	//	/// <summary>
+	//	/// Prints the given message to the console
+	//	/// </summary>
+	//	/// <param name="value"></param>
+	//	public static void LogIf(this IStratusLogger logger, bool condition, object value) => StratusDebug.LogIf(condition, value, logger, 2);
 
-		/// <summary>
-		/// Prints the given warning message to the console
-		/// </summary>
-		/// <param name="value"></param>
-		public static void LogWarning(this IStratusLogger logger, object value) => StratusDebug.LogWarning(value, logger, 2);
+	//	/// <summary>
+	//	/// Prints the given warning message to the console
+	//	/// </summary>
+	//	/// <param name="value"></param>
+	//	public static void LogWarning(this IStratusLogger logger, object value) => StratusDebug.LogWarning(value, logger, 2);
 
-		/// <summary>
-		/// Prints the given error message to the console
-		/// </summary>
-		/// <param name="value"></param>
-		public static void LogError(this IStratusLogger logger, object value) => StratusDebug.LogError(value, logger, 2);
+	//	/// <summary>
+	//	/// Prints the given error message to the console
+	//	/// </summary>
+	//	/// <param name="value"></param>
+	//	public static void LogError(this IStratusLogger logger, object value) => StratusDebug.LogError(value, logger, 2);
 
-		/// <summary>
-		/// Prints the given exception to the console
-		/// </summary>
-		/// <param name="value"></param>
-		public static void LogException(this IStratusLogger logger, Exception e) => StratusDebug.LogException(e);
+	//	/// <summary>
+	//	/// Prints the given exception to the console
+	//	/// </summary>
+	//	/// <param name="value"></param>
+	//	public static void LogException(this IStratusLogger logger, Exception e) => StratusDebug.LogException(e);
 
-		/// <summary>
-		/// Prints the given operation result to the console
-		/// </summary>
-		/// <param name="value"></param>
-		public static void Log(this IStratusLogger logger, StratusOperationResult result) => StratusDebug.Log(result, logger, 2);
-	}
+	//	/// <summary>
+	//	/// Prints the given operation result to the console
+	//	/// </summary>
+	//	/// <param name="value"></param>
+	//	public static void Log(this IStratusLogger logger, StratusOperationResult result) => StratusDebug.Log(result, logger, 2);
+	//}
 
 	public class UnityStratusLogger : StratusLogger
 	{
 		public override void LogError(string message)
 		{
-			Debug.LogError(message);
+			StratusDebug.LogError(message);
 		}
 
 		public override void LogInfo(string message)
 		{
-			Debug.Log(message);
+			StratusDebug.Log(message, 2);
 		}
 
 		public override void LogWarning(string message)
 		{
-			Debug.LogWarning(message);
+			StratusDebug.LogWarning(message);
+		}
+
+		public override void LogException(Exception ex)
+		{
+			StratusDebug.LogError(ex);
 		}
 	}
-
 }
