db463b47cc98f022f178265fb2fcbdbb4b22909d
Christian Sagel <ckpsm@live.com>
2023-02-25 18:21:32 +0100

    Move vector utility to NET

--- Runtime/Extensions/StratusVectorExtensions.cs
+++ Runtime/Extensions/StratusVectorExtensions.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 namespace Stratus
 {
-	public static partial class StratusExtensions
+	public static class UnityVectorExtensions
 	{
 		/// <summary>
 		/// Checks the specified value is within the range of this vector
@@ -143,53 +143,6 @@ namespace Stratus
 		public static Vector3 WithZ(this Vector3 vec, float z)
 		{
 			return new Vector3(vec.x, vec.y, z);
-		}
-
-		/// <summary>
-		/// Attempts to parse a Vector2 in either the format:
-		/// (a,b) or a,b
-		/// </summary>
-		public static Vector2 ParseVector2(string value)
-		{
-			// Remove the parentheses
-			if (value.StartsWith("(") && value.EndsWith(")"))
-			{
-				value = value.Substring(1, value.Length - 2);
-			}
-
-			// split the items
-			string[] values = value.Split(',');
-
-			// store as a Vector3
-			Vector2 result = new Vector2(
-				float.Parse(values[0]),
-				float.Parse(values[1]));
-
-			return result;
-		}
-
-		/// <summary>
-		/// Attempts to parse a Vector3 in either the format:
-		/// (a,b,c) or a,b,c
-		/// </summary>
-		public static Vector3 ParseVector3(string value)
-		{
-			// Remove the parentheses
-			if (value.StartsWith("(") && value.EndsWith(")"))
-			{
-				value = value.Substring(1, value.Length - 2);
-			}
-
-			// split the items
-			string[] values = value.Split(',');
-
-			// store as a Vector3
-			Vector3 result = new Vector3(
-				float.Parse(values[0]),
-				float.Parse(values[1]),
-				float.Parse(values[2]));
-
-			return result;
 		}
 
 		/// <summary>
